da47ed383167d6b019ca492abed318a5fd1fb9b6
Steve Gregory <esteve@howe.iplantc.org>
2012-04-10 15:46:15 -0700

    Updated caslib.py to include better README and documentation.

--- caslib.py
+++ caslib.py
@@ -1,14 +1,66 @@
 """
 caslib.py
 CAS Library for Python
-Contact:     Steven Gregory <esteve@iplantcollaborative.org>
+Contact: esteve <esteve@iplantcollaborative.org>
+
+Requirements:
+  CAS 2.0+ Server
+  httplib2
+
+Basic Authentication Scenario:
+  * Service wants to authenticate a user with CAS, once.
+
+Basic Usage:
+  * Web server acting as CAS Client needs two URLs:
+  {
+    login - This url is a landing page for the user. When they click login they should be redirected to :
+                 cas_server_url+"/cas/login?service="+cas_service_url
+
+    cas_service_url - The request landing at this URL will have a ticket, passed from CAS server.
+                      This page should call 'serviceValidate' and record the user as authorized.
+                      The cas_service_url should return the user to the correct locations depending on CAS validation.
+  }
+
+Advanced Authentication Scenario:
+  * Service wants to authenticate a user with CAS
+
+    AND
+
+  * After some time, service wants to ensure the same user is CAS authorized
+  * Service has 'authorized'/'protected' areas after initial login that require the user be CAS authorized
+  * Users are authenticated for some time, to get more time they must be CAS authorized
+  * Any other reason that a service must test user authenticity more than once
+
+Advanced Usage:
+  *** A Database (Or other method) to store and lookup (User,IOU & IOU,ID) will be required ***
+
+  *Web server acting as CAS Client needs four URLs:
+  {
+    login - This url is a landing page for the user. When they click [login] they should be redirected to :
+                 cas_server_url+"/cas/login?service="+cas_service_url
+
+    cas_service_url - The request landing at this URL will have a ticket in the query string (GET), passed from CAS server.
+                      This page should call 'serviceValidate' 
+                      This page should match:
+                        (User,IOU) from serviceValidate
+                       to (IOU,ID) from cas_proxy_url 
+                      The cas_service_url should always redirect user to the correct locations depending on CAS validation.
+
+    cas_proxy_callback - This is a dummy URL, required by CAS server. 
+    NOTE: <cas_proxy_callback> Must be on same server AND RSA or VeriSign SSL Certified
+
+    cas_proxy_url - The request landing at this URL will have (ProxyID, ProxyIOU) in the query string (GET), from CAS server.
+                    This page should record the ProxyID and ProxyIOU for immediate retrieval
+  }
+
+  * To programmatically determine user re-authorization: pass the last recorded proxyTicket for user to cas_proxyValidate(user, proxyTicket):
+    
 """
 import logging
 import httplib2
 
 #Global variables
 cas_server_url = cas_service_url = cas_proxy_url = cas_proxy_callback = ""
-
 def cas_init(server_url, service_url, proxy_url='', proxy_callback=''):
   """
   server_url, service_url : Initializes caslib authentication service
@@ -54,7 +106,7 @@ def cas_serviceValidate(ticket):
         return (False,username,"")
     return (False,username)
 
-def cas_reauthByProxy(user, proxyTicket):
+def cas_proxyValidate(user, proxyTicket):
     """
     Calls CAS using proxy to see what user is logged in
     returns true if the user matches parameter 'user' 
@@ -62,21 +114,24 @@ def cas_reauthByProxy(user, proxyTicket):
     global cas_server_url , cas_service_url , cas_proxy_url , cas_proxy_callback
     try:
         cas_proxy_url = cas_server_url+"/cas/proxy?targetService="+cas_proxy_callback+"&pgt="+proxyTicket
-        logging.info("cas_reauthByProxy /proxy URL:"+cas_proxy_url)
+        logging.info("cas_proxyValidate /proxy URL:"+cas_proxy_url)
         conn = httplib2.Http()
         (head,resp) = conn.request(cas_proxy_url)
         casticket = parse_tag(resp,"cas:proxyTicket")
         cas_valid_url = cas_server_url+"/cas/proxyValidate?ticket="+casticket+"&service="+cas_proxy_callback
-        logging.info("cas_reauthByProxy /proxyValidate URL:"+cas_valid_url)
+        logging.info("cas_proxyValidate /proxyValidate URL:"+cas_valid_url)
         (head,resp) = conn.request(cas_valid_url)
         casuser = parse_tag(resp,"cas:user")
     except Exception, e:
-        logging.error("cas_reauthByProxy Exception:"+str(e))
+        logging.error("cas_proxyValidate Exception:"+str(e))
         return False
 
     return (casuser == user)
 
 def parse_tag(str,tag):
+   """
+     Common helper method used to extract values from the tags from str
+   """
    tag1_pos1 = str.find("<" + tag)
    #  No tag found, return empty string.
    if tag1_pos1 == -1: return ""
