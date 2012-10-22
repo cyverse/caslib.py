530be9bc5a917eb9031b50b4cf641c0e13462079
Root Howe Antagonist <esteve@iplantcollaborative.org>
2012-10-22 10:58:32 -0700

    Returning the response in cas_proxyValidate to allow access to the LDAP attributes

--- caslib.py
+++ caslib.py
@@ -185,7 +185,7 @@ def cas_reauthenticate(user, proxyTicket):
     proxyUser = pv_response.map[pv_response.type].get('user','')
     logging.info("CAS ProxyUser:"+proxyUser)
 
-    return (user == proxyUser)
+    return ((user == proxyUser),pv_response.map)
 
 #Utiltiy Methods
 
