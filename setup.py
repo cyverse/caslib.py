abe64e05d71709d691296427a465d932a17f2521
Evan Briones <erbriones@gmail.com>
2015-01-26 12:07:40 -0700

    Update caslib.py for pypi.
    	new file:   AUTHORS
    	new file:   CHANGES
    	new file:   LICENSE
    	new file:   MANIFEST.in
    	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,12 @@
-#!/usr/bin/env python
-
-from distutils.core import setup
-"""
-def get_requirements(file_name):
-  f = open(file_name,'r')
-  for line in f.read.split('\n'):
-    if len(line) > 0 and "#" not in line[0]:
-      pass
-"""
+from setuptools import setup, find_packages
     
-setup(name='caslib',
-      version='2.0.0',
+setup(name='caslib.py',
+      version='2.1.0',
       description='CAS Client library',
-      author='Steven Gregory',
-      author_email='contact@steve-gregory.com',
+      author='iPlant Collaborative',
+      author_email='atmodevs@gmail.com',
       url='https://github.com/iPlantCollaborativeOpenSource/caslib.py',
       py_modules=['caslib'],
-      #packages=setuptools.find_packages(),
+      packages=find_packages(),
       install_requires = ['requests >= 2.2.1'],
      )
